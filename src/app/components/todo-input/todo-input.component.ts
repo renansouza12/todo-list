9370a7f1ce36dad23545c2f196e2e673cca192d3
Renan Souza <renansouza0122@gmail.com>
2024-07-10 02:10:18 -0300

    :lipstick: feat: menu animation

--- src/app/components/todo-input/todo-input.component.ts
+++ src/app/components/todo-input/todo-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 @Component({
@@ -9,21 +9,35 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './todo-input.component.scss'
 })
 export class TodoInputComponent {
-
   title:string = "title";
   desc!:string;
   titleStep:boolean = true;
   descStep!:boolean;
   @Input() displayMenu:boolean = false;
+  anim!:string;
+  animDesc!:string;
+
 
   next():void{
     console.log("clicked", this.title);
-    this.titleStep = false;
-    this.descStep = true;
+   
+    this.anim = "item";
+
+    setTimeout(() => {
+      this.titleStep = false;
+      this.descStep = true;
+      this.anim = "";
+      this.animDesc = "desc";
+    }, 1000);
+
   }
   previous():void{
-    this.titleStep = true;
-    this.descStep = false;
+    this.anim = "item";
+      setTimeout(() => {
+         this.descStep = false;
+         this.titleStep = true;
+         this.anim = "appear";
+      }, 2000);
   }
   done():void{
     console.log(this.desc);
